dafacc32b729a08250d80db5dae9a4da03d54162
AidenEyre <EyreAiden@gmail.com>
2024-03-09 23:36:18 -0700

    feat: Add config view option

--- cmd/config/cli_config_setup.go
+++ cmd/config/cli_config_setup.go
@@ -8,18 +8,23 @@ import (
 	"github.com/aideneyre/acli/internal/config"
 	"github.com/aideneyre/acli/internal/config/configurator"
 	"github.com/manifoldco/promptui"
+	"gopkg.in/yaml.v2"
 
 	"github.com/spf13/cobra"
 )
 
 var (
 	ConfigCmd = &cobra.Command{
-		Use:   "config [flags]",
-		Short: "Configure acli settings.",
-		Long:  "Opens an interactive prompt to configure acli settings for select subcommands: kch, connect.",
+		Use:       "config [flags] [options]",
+		Short:     "Configure acli settings.",
+		Long:      "Opens an interactive prompt to configure acli settings for select subcommands: kch, connect.",
+		ValidArgs: []string{"view"},
 		Example: `
 # Open the configuration prompt
 acli config
+
+# View the existing configuration
+acli config view
 		`,
 		Version:      "0.1.0",
 		RunE:         runE,
@@ -34,6 +39,10 @@ func runE(cmd *cobra.Command, args []string) error {
 		return fmt.Errorf("unable to initialize acli: %w", err)
 	}
 
+	if len(args) == 1 && args[0] == "view" {
+		return printConfig()
+	}
+
 	err = configurator.RunSetup()
 	if errors.Is(err, promptui.ErrInterrupt) {
 		fmt.Print("\033[u\033[J") // Clear the terminal
@@ -44,5 +53,21 @@ func runE(cmd *cobra.Command, args []string) error {
 		return fmt.Errorf("unable to configure acli: %w", err)
 	}
 
+	return nil
+}
+
+func printConfig() error {
+	var yamlConfig interface{}
+	err := config.UnmarshalConfig(&yamlConfig)
+	if err != nil {
+		return fmt.Errorf("unable to unmarshal config: %w", err)
+	}
+
+	yamlBytes, err := yaml.Marshal(&yamlConfig)
+	if err != nil {
+		return fmt.Errorf("unable to marshal config to YAML: %w", err)
+	}
+
+	fmt.Println(string(yamlBytes))
 	return nil
 }
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/spf13/cobra v1.6.1
 	github.com/spf13/viper v1.18.2
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9
+	gopkg.in/yaml.v2 v2.4.0
 	k8s.io/client-go v0.28.1
 )
 
@@ -46,7 +47,6 @@ require (
 	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/apimachinery v0.28.1 // indirect
 	k8s.io/klog/v2 v2.100.1 // indirect
